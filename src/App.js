7893a0c602c964a11d719e8b284581c69c6a8581
omar <jlopezal@hamilton.edu>
2022-09-26 13:39:54 -0400

    Bang time

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+
+import React from 'react';
+import Search from './components/Search';
+import initialDetails from './data/initialDetails';
+
+function App() {
+  return (
+    <div className="tc bg-white ma0 pa4 min-vh-100">
+      <Search details={initialDetails}/>
+    </div>
+  );
+
+  
+}
+
+export default App;
