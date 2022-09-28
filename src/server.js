8fede4d7c37616f8500e2e7bee25b9c2d765fe57
ronnaksaxena <rsaxena@hamilton.edu>
2022-09-28 10:42:58 -0400

    Tried to setup express

---
+++ src/server.js
@@ -0,0 +1,10 @@
+// // Tryna get express to work
+// const express = require("express")
+// const app = express()
+
+// app.get("/", (req, res)) => {
+//   console.log("Here")
+//   res.send("Hi")
+// })
+
+// app.listen(3000)
